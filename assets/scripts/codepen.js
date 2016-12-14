f08590141c01d2d528de5fe4ccb5f3b72d92017d
Sebastian Prein <basti@gridonic.ch>
2016-12-14 15:24:59 +0100

    Add dynamic loading of JS files to codepen script

--- assets/scripts/codepen.js
+++ assets/scripts/codepen.js
@@ -1,4 +1,27 @@
-// @see https://highlightjs.org/usage/
-if (hljs) {
+var JavaScript = {
+    load: function(url, callback) {
+        var script = document.createElement('script')
+        script.type = 'text/javascript';
+
+        if (script.readyState) { // IE
+            script.onreadystatechange = function() {
+                if (script.readyState == 'loaded' || script.readyState == 'complete') {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else { // Others
+            script.onload = function() {
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+};
+
+// @see https://highlightjs.org/
+JavaScript.load('//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js', function() {
     hljs.initHighlightingOnLoad();
-}
+});
