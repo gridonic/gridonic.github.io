6dbd7fe605555e3f9cac30f94fbd1908e5f0beb1
Sebastian Prein <basti@gridonic.ch>
2017-03-28 14:42:24 +0200

    Add new index page

---
+++ assets/scripts/main.js
@@ -0,0 +1,17 @@
+document.querySelector('[data-emoji]').textContent = (
+    function(emojis) {
+        return emojis[Math.floor(Math.random() * emojis.length)];
+    }
+)(['💪', '❤️', '👏', '🙏', '🔥', '🤘', '👍', '🚀']);
+
+var links = document.querySelectorAll('.link-primary');
+
+for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    var b = baffle(link, {
+        speed: 60
+    });
+
+    link.addEventListener('mouseenter', function() { this.start(); }.bind(b));
+    link.addEventListener('mouseleave', function() { this.reveal(1000); }.bind(b));
+}
