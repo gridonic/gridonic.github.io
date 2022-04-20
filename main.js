f921fededcc61ce3f2a167e976eeb737338f7007
Christian <christianmoser88@gmail.com>
2022-04-20 08:28:28 +0200

    refactor: create global app component

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'petite-vue';
 import './src/css/main.css';
-import DarkMode from './src/js/components/DarkMode.js';
+import App from './src/js/components/App.js';
 
 createApp({
-  DarkMode
+  App
 }).mount();
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,8 +1,12 @@
-export default function DarkMode() {
+export default function App() {
   return {
     theme: 'light',
     changeTheme() {
       this.theme = this.theme === 'dark' ? 'light' : 'dark';
+    },
+    get year() {
+      const now = new Date;
+      return now.getFullYear();
     }
   }
 }
